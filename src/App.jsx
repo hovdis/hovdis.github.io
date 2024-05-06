bbee17de901adeab0f6af34934108220a047308e
hovde <rune.hovde@entur.org>
2024-05-06 10:53:12 +0200

    remove the routing

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,16 @@ import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
-  const router = createBrowserRouter(
+  <h1>test testersen</h1>;
+  /*   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
-    )
+    ) 
   );
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;*/
 };
 
 export default App;
